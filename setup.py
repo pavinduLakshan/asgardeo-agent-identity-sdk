7ad9b6df62dcdb4390937e36a1239b6097727a0e
Pavindu Lakshan <pavindulakshan@gmail.com>
2025-04-11 14:44:01 +0530

    Init project

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='agent-identity-sdk',
+    version='0.1.0',
+    description='Build seamless identity-aware integrations for LLM agents',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='WSO2',
+    author_email='asgardeo-help@wso2.com',
+    url='https://github.com/wso2/agent-identity-sdk',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.7',
+    install_requires=[],
+)
